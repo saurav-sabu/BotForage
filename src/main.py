3f78a9e248c11a8ae8498651ce2bb9ec06a322bf
saurav-sabu <saurav.sabu9@gmail.com>
2025-09-20 22:12:27 +0530

    solved cors issue

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from src.db.mongodb import init_db
-from src.routes import auth_routes, llm_routes
+from src.routes import auth_routes, llm_routes #, file_routes, prompt_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create an instance of the FastAPI application
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 # Event handler to initialize the database connection when the application starts
 @app.on_event("startup")
@@ -16,3 +26,8 @@ app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
 
 # Include the LLM configs (Language Learning Model) routes with a prefix and tag for better organization
 app.include_router(llm_routes.router1, prefix="/llm", tags=["LLM"])
+
+# Include the file routes with a prefix and tag for better organization
+# app.include_router(file_routes.router2, prefix="/file", tags=["File"])
+
+# app.include_router(prompt_routes.router3,prefix="/prompt",tags=["Prompt"])
